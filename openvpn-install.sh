993583b923179b24e2e0004ae15a00e5205a100a
NajibNour <39351019+NajibNour@users.noreply.github.com>
2018-11-14 13:26:45 -0500

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 #
-# https://github.com/Nyr/openvpn-install
 #
-# Copyright (c) 2013 Nyr. Released under the MIT License.
 
-
-# Detect Debian users running the script with "sh" instead of bash
 if readlink /proc/$$/exe | grep -q "dash"; then
 	echo "This script needs to be run with bash, not sh"
 	exit
